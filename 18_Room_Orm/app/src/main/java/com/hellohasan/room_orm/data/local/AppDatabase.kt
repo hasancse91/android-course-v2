504eecf16b195d0aca2629f338dc22c543b567fb
Hasan Abdullah <hasan.cse91@gmail.com>
2020-12-03 20:00:03 +0600

    dead code removed

--- 18_Room_Orm/app/src/main/java/com/hellohasan/room_orm/data/local/AppDatabase.kt
+++ 18_Room_Orm/app/src/main/java/com/hellohasan/room_orm/data/local/AppDatabase.kt
@@ -1,8 +1,6 @@
 package com.hellohasan.room_orm.data.local
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.hellohasan.room_orm.data.repository.student.Student
 import com.hellohasan.room_orm.data.repository.subject.Subject
@@ -12,13 +10,4 @@ abstract class AppDatabase : RoomDatabase() {
 
     abstract fun studentDao(): StudentDao
     abstract fun subjectDao(): SubjectDao
-
-    companion object {
-        fun getInstance(context: Context): AppDatabase {
-            return Room.databaseBuilder(
-                context,
-                AppDatabase::class.java, "student-db"
-            ).build()
-        }
-    }
 }
