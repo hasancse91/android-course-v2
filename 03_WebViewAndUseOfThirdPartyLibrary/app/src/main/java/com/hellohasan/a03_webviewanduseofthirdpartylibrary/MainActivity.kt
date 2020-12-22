ee49f23091fd5bd933209cb540d70855a0ddc2fb
Hasan Abdullah <hasan.cse91@gmail.com>
2020-12-22 11:12:24 +0600

    webview project implemented

--- 03_WebViewAndUseOfThirdPartyLibrary/app/src/main/java/com/hellohasan/a03_webviewanduseofthirdpartylibrary/MainActivity.kt
+++ 03_WebViewAndUseOfThirdPartyLibrary/app/src/main/java/com/hellohasan/a03_webviewanduseofthirdpartylibrary/MainActivity.kt
@@ -1,21 +1,34 @@
 package com.hellohasan.a03_webviewanduseofthirdpartylibrary
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import com.orhanobut.logger.AndroidLogAdapter
 import com.orhanobut.logger.Logger
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Logger.addLogAdapter(AndroidLogAdapter())
 
-        Logger.e("hello")
+        Logger.d("This is a Pretty Log message")
 
-//        Log.d("tag", "=============\n\nmessage\n\n================")
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
+        }
+        webView.webChromeClient = WebChromeClient()
+        webView.loadUrl("https://hellohasan.com")
     }
 }
